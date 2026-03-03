9ecfc67aace8c2ffd04c828b5585f8484512a57b
Edith <gmirandab2425@politecnics.barcelona>
2026-03-03 20:20:45 +0100

    Creación de el metodo post en la api para insertar usuarios en la base de datos.

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/users', [UserController::class, 'index']);
+Route::post('/register/client', [AuthController::class, 'registerClient']);
+
+
