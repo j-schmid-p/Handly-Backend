bf71d5d74f78d42aa390c8ce0116ad094df5855a
Edith <gmirandab2425@politecnics.barcelona>
2026-03-09 20:19:33 +0100

    Creacion de endpoint de resgistro de  profesionales y creacion de tokens

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,13 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/register/client', [AuthController::class, 'registerClient']);
+Route::post('/register/professional', [AuthController::class, 'registerProfessional']);
+Route::post('/login', [AuthController::class, 'login']);
+// rutas protegidas (acceso mediante token)
+Route::middleware('auth:sanctum')->group(function (){
+    Route::get('/perfil', function (Request $request){
+        return $request->user();
+    });
+});
 
 
