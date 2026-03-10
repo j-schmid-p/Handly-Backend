25d627c4a3c6051a05e1bb97a3977eee9d9fad01
Edith <gmirandab2425@politecnics.barcelona>
2026-03-10 21:11:47 +0100

    Creación del controlador para profesiones y el logout

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProfessionController;
 
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/register/client', [AuthController::class, 'registerClient']);
 Route::post('/register/professional', [AuthController::class, 'registerProfessional']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/professions',[AuthController::class, 'index']);
+
+
 // rutas protegidas (acceso mediante token)
 Route::middleware('auth:sanctum')->group(function (){
     Route::get('/perfil', function (Request $request){
         return $request->user();
     });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 
