25f6b8b0470897c068bcb0f714c1dffaf34c84e3
Edith <gmirandab2425@politecnics.barcelona>
2026-03-10 23:47:49 +0100

    Creacion de el endpoint de busqueda de profesionales mediante id y actualizacion de la funcion de busqueda de profesionales por profesion

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfessionController;
+use App\Http\Controllers\ProfessionalController;
 
 Route::get('/users', [UserController::class, 'index']);
 Route::post('/register/client', [AuthController::class, 'registerClient']);
@@ -15,11 +16,15 @@ Route::get('/professions',[AuthController::class, 'index']);
 
 // rutas protegidas (acceso mediante token)
 Route::middleware('auth:sanctum')->group(function (){
+
     Route::get('/perfil', function (Request $request){
         return $request->user();
     });
 
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/professionals', [AuthController::class, 'index']);
+    Route::get('/professionals/{id}', [AuthController::class, 'show']);
+
 });
 
 
